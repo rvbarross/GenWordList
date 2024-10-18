dabc5f09ec7ef94e12e295c86d310f71f9cc83dc
Vitor Barros <70072750+rvbarross@users.noreply.github.com>
2024-10-18 11:44:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from openai import OpenAI
+
+# Ignorar função por enquanto porque para utilizar API do chat precisa pagar!
 def call_chatgpt_api(api_key, prompt, model="gpt-4o-mini"):
 
     url = "https://api.openai.com/v1/chat/completions"
@@ -22,9 +24,29 @@ def call_chatgpt_api(api_key, prompt, model="gpt-4o-mini"):
     else:
         return f"Erro {response.status_code}: {response.text}"
 
+def call_ollama(prompt, model="llama3.2"):
+
+    url = "http://localhost:11434/api/generate"
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "model": model,
+        "prompt": prompt
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        print(response )
+    except requests.exceptions.RequestException as e:
+        return f"Erro de conexão: {e}"
+
 # Exemplo de uso
 api_key = ""
 prompt = "Me dê uma recomendação de livro de ficção científica."
 
-resposta = call_chatgpt_api(api_key, prompt)
+resposta = call_ollama(prompt)
 print(resposta)
+
